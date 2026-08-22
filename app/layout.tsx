881a06c2a7eed690e078ea8a73fd95a08625fc85
Ammar Yaser <ammar.alhwamdah@gmail.com>
2026-08-23 00:18:13 +0300

    remove duplicate GA4 from layout - GTM handles it

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,13 +84,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <head>
         <script dangerouslySetInnerHTML={{ __html: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PC37BFTH');" }} />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GR5DG65ZK7" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html:
-              "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-GR5DG65ZK7');",
-          }}
-        />
         <script dangerouslySetInnerHTML={{ __html: "window.maeExportApis_=window.maeExportApis_||function(){};" }} />
         <script
           dangerouslySetInnerHTML={{
