505b83b2d32c8c54a5d36681afa61be46523d447
Ammar Yaser <ammar.alhwamdah@gmail.com>
2026-09-16 15:37:41 +0300

    Self-host gtag.js on playkrux.com to bypass adblockers blocking googletagmanager

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <head>
         <meta name="msvalidate.01" content="0260A71124945E310E69BC523F086B61" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GR5DG65ZK7"></script>
+        <script async src="/gtag.js?id=G-GR5DG65ZK7"></script>
         <script
           dangerouslySetInnerHTML={{
             __html:
